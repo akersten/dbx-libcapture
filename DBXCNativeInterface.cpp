ed691b2a83eb2281c669347e5024eae718b8c3ba
Alex Kersten <alex@dividebyxero.com>
2013-06-03 00:24:23 -0500

    Framework for native code

---
+++ DBXCNativeInterface.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <windows.h>
+
+#include "DBXCNativeInterface.h"
+
+JNIEXPORT void JNICALL
+Java_com_dividebyxero_dbxcapture_jni_DBXCNativeInterface_debugDumpBuffer
+(JNIEnv * env, jclass clazz) {
+
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_dividebyxero_dbxcapture_jni_DBXCNativeInterface_waitForScreenshotActionAndReturnPath
+(JNIEnv * env, jclass clazz, jstring _CONTENTDIRECTORY, jint _TRIPCODE) {
+
+}
